6ca1939803aa37df4e9c2a6a0a51e54a9faeb401
DanielHernandezO <daniel.hernandez.54361@gmail.com>
2023-02-21 21:00:52 -0500

    added the 10th activity

--- firstTutorial/resources/views/layouts/app.blade.php
+++ firstTutorial/resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
                   <a class="nav-link active" href="{{route('home.about')}}">About</a>
                   <a class="nav-link active" href="{{route('home.contact')}}">Contact</a>
                   <a class="nav-link active" href="{{route('product.index')}}">Products</a>
+                  <a class="nav-link active" href="{{route('product.create')}}">Create</a>
                </div>
             </div>
          </div>
@@ -42,7 +43,7 @@
          <div class="container">
             <small>
             Copyright - <a class="text-reset fw-bold text-decoration-none" target="_blank"
-               href="https://twitter.com/danielgarax">Daniel Correa
+               href="https://twitter.com/danielgarax">Daniel Correa - Daniel Hernandez
             </a>
             </small>
          </div>
